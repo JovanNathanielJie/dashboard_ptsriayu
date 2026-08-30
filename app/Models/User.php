c3e12708ec003bf8eef8fc37a703ba1a16884493
JovanNathanielJie <jovannathanieljie_2327240013@mhs.mdp.ac.id>
2026-08-30 20:13:12 +0700

    Migration Role User

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,11 +18,12 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'role',   // <-- tambahkan ini
+];
 
     /**
      * The attributes that should be hidden for serialization.
